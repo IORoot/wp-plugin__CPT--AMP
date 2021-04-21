780913f662f8cff1889abdac1fdc15381826a1d0
Andy Pearson <andy@londonparkour.com>
2021-04-21 17:17:49 +0100

    trim

--- src/views/template-parts/amp_header.php
+++ src/views/template-parts/amp_header.php
@@ -1,6 +1,3 @@
-<?php
-    $body_classes = get_field('body_classes'); 
-?>
 <!doctype html>
 <html ⚡ lang="en-uk">
     <head>
@@ -13,7 +10,6 @@
         <!-- Import other AMP Extensions here -->
         
         <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
-        <noscript><style amp-boilerplate=""> body { -webkit-animation: none; -moz-animation: none; -ms-animation: none; animation: none; } </style></noscript>
         
         <style amp-custom>
             <?php 
@@ -36,7 +32,7 @@
         <?php include( __DIR__ . '/amp_schema.php'); ?>
 
     </head>
-    <body <?php body_class('amp londonparkour ' . $body_classes );?> >
+    <body <?php body_class('amp londonparkour ' . get_field('body_classes') );?> >
 
         <?php include( __DIR__ . '/amp_sidemenu.php'); ?>
         <?php include( __DIR__ . '/amp_navigation.php'); ?>
